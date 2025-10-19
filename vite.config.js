75142d717f58fa5162c1c687cda44e6d703b732d
Silvineey <lucreciasilvio@hotmail.com>
2025-10-19 09:56:36 -0300

    salvar

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react({
-      babel: {
+      base:"To-do-List2.0", 
         plugins: [['babel-plugin-react-compiler']],
       },
-    }),
+    ),
   ],
 })
